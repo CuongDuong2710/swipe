b9450c4080ed2426b87025e548e301ff5b10365a
CuongDuong2710 <cuongduongngocquoc@gmail.com>
2017-08-22 01:16:18 +0700

    add react-native-elements library and Card section

--- src/Deck.js
+++ src/Deck.js
@@ -2,9 +2,18 @@ import React, { Component } from 'react'
 import { View, Animated } from 'react-native'
 
 class Deck extends Component {
+// Take a list of data and for every element in that array it calls render card.
+  renderCard () {
+    console.log(this.props)
+    return this.props.data.map(item => {
+      return this.props.renderCard(item)
+    })
+  }
   render () {
     return (
-      <View />
+      <View>
+        {this.renderCard()}
+      </View>
     )
   }
 }
