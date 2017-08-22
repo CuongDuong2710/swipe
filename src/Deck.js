69e7baffa340fb6d1e3c5a016644a4999b942792
CuongDuong2710 <cuongduongngocquoc@gmail.com>
2017-08-22 11:30:44 +0700

    adding panHandlers callbacks

--- src/Deck.js
+++ src/Deck.js
@@ -9,6 +9,7 @@ class Deck extends Component {
   constructor (props) {
     super(props)
 
+    // It means that we want this instance of the responder to be responsible for the user pressing on the screen.
     const panResponder = PanResponder.create({
       onStartShouldSetPanResponder: () => true,
       onPanResponderMove: (event, gesture) => {
@@ -28,9 +29,10 @@ class Deck extends Component {
     })
   }
 
+  // 'panHandlers' is an object has a bunch of different callbacks that help intercept (ngăn lại) presses from a user by using the dot dot dot syntax right here. 
   render () {
     return (
-      <View>
+      <View {...this.state.panResponder.panHandlers}>
         {this.renderCard()}
       </View>
     )
