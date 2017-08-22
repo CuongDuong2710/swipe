7e48cb2620000e09d6ef53d838a4a9c87d1824ee
CuongDuong2710 <cuongduongngocquoc@gmail.com>
2017-08-22 10:32:43 +0700

    add panResponder for user pressing on the screen

--- src/Deck.js
+++ src/Deck.js
@@ -1,14 +1,33 @@
 import React, { Component } from 'react'
-import { View, Animated } from 'react-native'
+import {
+  View,
+  Animated,
+  PanResponder
+} from 'react-native'
 
 class Deck extends Component {
-// Take a list of data and for every element in that array it calls render card.
+  constructor (props) {
+    super(props)
+
+    const panResponder = PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onPanResponderMove: (event, gesture) => {
+        console.log(gesture)
+      },
+      onPanResponderRelease: () => {}
+    })
+
+    this.state = { panResponder }
+  }
+
+  // Take a list of data and for every element in that array it calls render card.
   renderCard () {
     console.log(this.props)
     return this.props.data.map(item => {
       return this.props.renderCard(item)
     })
   }
+
   render () {
     return (
       <View>
